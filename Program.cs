f43326818c3be17f379d7e49a5aeed52a34727d3
Ihsan-Ahmad Olamide Sodeko <ihsan.ahmadsodeko@gmail.com>
2025-05-14 18:29:00 +0100

    Interface and testing practice

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace InterfaceAndTesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
