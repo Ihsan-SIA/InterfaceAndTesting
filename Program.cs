7fafc121d8a940522fea9e3b49e1282cc7bffa52
Ihsan-Ahmad Olamide Sodeko <ihsan.ahmadsodeko@gmail.com>
2025-05-27 15:54:28 +0100

    Just trying to practice interfaces

--- Program.cs
+++ Program.cs
@@ -24,3 +24,23 @@ public class Hojlund : IStriker
         Console.WriteLine("I made 5 assists this season");
     }
 }
+public class Mbappe : IStriker
+{
+    public void DefendIfPossible()
+    {
+        Console.WriteLine("I prevented 1 goal this season indirectly");
+    }
+
+    public void ScoreGoals()
+    {
+        Console.WriteLine("I scored 55 goals this season");
+        Console.WriteLine("Twelve of them were headers");
+        Console.WriteLine("Four free kicks");
+    }
+
+    public void SomeAssists()
+    {
+        Console.WriteLine("I made 20 assists too");
+        Console.WriteLine("11 of them went to Vini Jr");
+    }
+}
